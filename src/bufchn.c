37f5c581955a303b3cf491296816ea17f9f8fa6e
davnpsh <me@davnpsh.dev>
2026-04-17 01:31:48 -0500

    clarify: TODO

--- src/bufchn.c
+++ src/bufchn.c
@@ -78,14 +78,12 @@ void buf_insert(BUFFER_CHAIN *buf_chain, int line_num, int offset, char *new)
 	
 	if (offset > buf_node->len) return;
 	
-	BUFFER_NODE *last = buf_node;
+	BUFFER_NODE *prev = buf_node;
+	BUFFER_NODE *last = buf_node->next;
 	
 	for (int i = 0; *new != '\0'; i++, new++)
 	{
-		// if ()
-		// {
-			
-		// }
+		// TODO: Implement this!
 	}
 }
 
